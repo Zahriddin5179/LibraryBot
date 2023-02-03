db2e7f23398b6ab28931f1253243ea2129221b90
Zahriddin5179 <zakhiriddinkhasanov013@gmail.com>
2023-02-04 01:35:53 +0500

    first commit

---
+++ src/main/java/dev/zahriddin/librarybot/LibraryBotApplication.java
@@ -0,0 +1,13 @@
+package dev.zahriddin.librarybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBotApplication.class, args);
+    }
+
+}
